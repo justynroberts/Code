7badfc978eb9052cc19f5c50e01087accb59b887
Michele Castellana <michele.castellana@jpmorgan.com>
2024-11-20 14:20:27 +0000

    Turns out I need a post not a get

--- myflask.py
+++ myflask.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import logging
 
 APP = Flask(__name__)
@@ -9,6 +9,12 @@ LOGGER = logging.getLogger(__name__)
 def hello_world():
     LOGGER.info("This function is called")
     return {"message": "My webhook for the PD POC"}, 200
+
+@APP.route('/mypost', methods=['POST'])
+def post_some_data():
+    data = request.get_json()
+    LOGGER.info("Received data: %s", data)
+    return jsonify({"message": "Data received", "data": data}), 200
 
 if __name__ == '__main__':
     APP.run(debug=True)
